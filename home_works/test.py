25af31dd9de7fe4f1410adf59f678f7c71411629
Pavel7334 <novaya.rabota.pavla2016@yandex.ru>
2024-02-28 11:48:52 +0400

    Домашнее задание №18

--- home_works/test.py
+++ home_works/test.py
@@ -5,4 +5,9 @@ pattern = r'\+?7\d{10}'
 
 print(re.findall(pattern, s))
 
+import re
 
+s = "+ 7 499 456-45-78, +74994564578, 7 (499) 456 45 78, 74994564578"
+pattern = r'\+?7\d{10}|\(?\d{3}\)?[ -]?\d{2}(?:[ -]?\d{2}){2}'
+
+print(re.findall(pattern, s))
