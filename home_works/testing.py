f41d3707e7597b102dbc10661450325529d0d2a0
Pavel7334 <novaya.rabota.pavla2016@yandex.ru>
2024-03-26 14:23:50 +0400

    Домашнее задание №26

--- home_works/testing.py
+++ home_works/testing.py
@@ -2,46 +2,40 @@ import math
 
 
 class Pair:
+
     def __init__(self, a: float, b: float):
-        self.A = a  # Первый катет
-        self.B = b  # Второй катет
+        self.A = a
+        self.B = b
 
     def change_numbers(self, a: float, b: float) -> None:
-        """Метод для изменения значений катетов A и B."""
         self.A = a
         self.B = b
 
     def product(self) -> float:
-        """Метод для вычисления произведения катетов A и B."""
         return self.A * self.B
 
     def sum(self) -> float:
-        """Метод для вычисления суммы катетов A и B."""
         return self.A + self.B
 
 
 class RightTriangle(Pair):
+
     def __init__(self, a: float, b: float):
         super().__init__(a, b)
 
     def hypotenuse(self) -> float:
-        """Метод для вычисления длины первой гипотенузы прямоугольного треугольника."""
         return math.sqrt(self.A ** 2 + self.B ** 2)
 
     def second_hypotenuse(self) -> float:
-        """Метод для вычисления длины второй гипотенузы прямоугольного треугольника."""
         return math.sqrt(self.A ** 2 + self.B ** 2 + (2 * self.A * self.B))
 
     def area(self) -> float:
-        """Метод для вычисления площади прямоугольного треугольника."""
         return 0.5 * self.A * self.B
 
 
-# Пример использования классов
 pair1 = Pair(5, 8)
 triangle1 = RightTriangle(pair1.A, pair1.B)
 
-# Вывод информации о прямоугольном треугольнике
 print("Гипотенуза △ABC:", "{:.2f}".format(triangle1.hypotenuse()))
 print("Прямоугольный треугольник △ABC ({}, {}, {:.2f})".format(triangle1.A, triangle1.B, triangle1.hypotenuse()))
 print("Площадь △ABC:", "{:.1f}".format(triangle1.area()))
@@ -53,7 +47,6 @@ print()
 pair2 = Pair(15, 20)
 triangle2 = RightTriangle(pair2.A, pair2.B)
 
-# Вывод информации о прямоугольном треугольнике
 print("Гипотенуза △ABC:", "{:.2f}".format(triangle2.hypotenuse()))
 print("Гипотенуза △ABC:", "{:.2f}".format(triangle2.second_hypotenuse()))
 print("Сумма:", pair2.sum())
