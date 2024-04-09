dac29511022366f8f2ab39047f7852db044a93d7
Pavel7334 <novaya.rabota.pavla2016@yandex.ru>
2024-04-09 13:12:02 +0400

    Домашнее задание №30 (добавлены исправление кода)

--- home_works/home_work30/main.py
+++ home_works/home_work30/main.py
@@ -1,24 +1,10 @@
-from typing import List
-
-from home_works.home_work30.employee import Employee
-
-
-class PayrollSystem:
-    """Система расчета заработной платы"""
-
-    def calculate(self, employees: List[Employee]) -> None:
-        print("Расчет заработной платы")
-        print("=" * 50)
-        for employee in employees:
-            print(f"Заработная плата: {employee.id} - {employee.name}")
-            print(f"- Проверить сумму: {employee.calculate_payroll()}")
-            print()
-
 
 if __name__ == '__main__':
+
     from salary_employee import SalaryEmployee
     from hourly_employee import HourlyEmployee
     from comission_employee import CommissionEmployee
+    from payroll_system import PayrollSystem
 
     salary_employee = SalaryEmployee(1, "Валерий Задорожный", 1500)
     hourly_employee = HourlyEmployee(2, "Илья Кромин", 40, 15)
---
+++ home_works/home_work30/payroll_system.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from home_works.home_work30.employee import Employee
+
+
+class PayrollSystem:
+    """Система расчета заработной платы"""
+
+    def calculate(self, employees: List[Employee]) -> None:
+        print("Расчет заработной платы")
+        print("=" * 50)
+        for employee in employees:
+            print(f"Заработная плата: {employee.id} - {employee.name}")
+            print(f"- Проверить сумму: {employee.calculate_payroll()}")
+            print()
