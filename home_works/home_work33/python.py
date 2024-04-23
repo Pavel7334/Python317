95801bed329ea257a8a414257cf6307a2af5a340
Pavel7334 <novaya.rabota.pavla2016@yandex.ru>
2024-04-23 13:37:44 +0400

    Домашнее задание №34

--- home_works/home_work33/python.py
+++ home_works/home_work33/python.py
@@ -53,3 +53,27 @@ todos_data = fetch_todos(todos_url)
 csv_filename = "todos.csv"
 write_todos_to_csv(todos_data, csv_filename)
 
+#                                           Версия 2.0
+
+# import requests
+# import json
+# import csv
+#
+# # Получаем данные о задачах с удаленного сервера
+# response = requests.get("https://jsonplaceholder.typicode.com/todos")
+# todos = json.loads(response.text)
+#
+# # Определяем заголовки CSV файла
+# csv_columns = ['userId', 'id', 'title', 'completed']
+#
+# # Открываем CSV файл для записи
+# with open('todos.csv', 'w', newline='') as csvfile:
+#     writer = csv.DictWriter(csvfile, delimiter=";", fieldnames=csv_columns)
+#
+#     # Записываем заголовки
+#     writer.writeheader()
+#
+#     # Записываем данные
+#     for todo in todos:
+#         writer.writerow(todo)
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import charset_normalizer
+
 import geometry
 from datetime import datetime
 
@@ -5883,18 +5885,166 @@ from bs4 import BeautifulSoup
 #
 # print(copywriter)
 
+# import re
+#
+#
+# def get_salary(s):
+#     pattern = r"\d+"
+#     res = re.search(pattern, s).group()
+#     print(res)
+#
+#
+# f = open("index.html").read()
+# soup = BeautifulSoup(f, "html.parser")
+# salary = soup.find_all("div", {"data-set": "salary"})
+#
+# for i in salary:
+#     get_salary(i.text)
+
+import requests
+from bs4 import BeautifulSoup
 import re
 
+# r = requests.get("https://ru.wordpress.org/")
+# print(r)
+# print(r.status_code)
+# print(r.headers['Content-Type'])
+# print(r.content)
+# print(r.headers)
+# print(r.text)
 
-def get_salary(s):
-    pattern = r"\d+"
-    res = re.search(pattern, s).group()
-    print(res)
+
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def get_data(html):
+#     soup = BeautifulSoup(html, "lxml")
+#     p1 = soup.find("header", id="masthead").find("p", class_="site-title").text
+#     return p1
+#
+#
+# def main():
+#     url = "https://ru.wordpress.org/"
+#     print(get_data(get_html(url)))
+#
+#
+# if __name__ == '__main__':
+#     main()
 
 
-f = open("index.html").read()
-soup = BeautifulSoup(f, "html.parser")
-salary = soup.find_all("div", {"data-set": "salary"})
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def refined(s):
+#     res = re.sub(r"\D+", "", s)
+#     return res
+#
+#
+# def write_csv(data):
+#     with open("plugins.csv", "a") as f:
+#         writer = csv.writer(f, delimiter=";", lineterminator="\r")
+#         writer.writerow((data["name"], data["url"], data["rating"]))
+#
+#
+# def get_data(html):
+#     soup = BeautifulSoup(html, "lxml")
+#     p1 = soup.find_all("section", class_="plugin-section")[-1]
+#     plugins = p1.find_all("article")
+#
+#     for plugin in plugins:
+#         name = plugin.find("h3").text
+#         url = plugin.find("h3").find("a").get("href")
+#         rating = plugin.find("span", class_="rating-count").find("a").text
+#         r = refined(rating)
+#         data = {"name": name, "url": url, "rating": r}
+#         write_csv(data)
+#
+#
+# def main():
+#     url = "https://ru.wordpress.org/plugins/"
+#     print(get_data(get_html(url)))
+#
+#
+# if __name__ == '__main__':
+#     main()
 
-for i in salary:
-    get_salary(i.text)
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def refine_cy(s):
+#     return s.split()[-1]
+#
+#
+# def get_data(html):
+#     soup = BeautifulSoup(html, "lxml")
+#     elements = soup.find_all("article", class_="plugin-card")
+#
+#     for el in elements:
+#         try:
+#             name = el.find("h3").text
+#         except AttributeError:
+#             name = ""
+#
+#         try:
+#             url = el.find("h3").find("a")["href"]
+#         except AttributeError:
+#             url = ""
+#
+#         try:
+#             snippet = el.find("div", class_="entry-excerpt").text.strip()
+#         except AttributeError:
+#             snippet = ""
+#
+#         try:
+#             active = el.find("span", class_="active-installs").text.strip()
+#         except AttributeError:
+#             active = ""
+#
+#         try:
+#             c = el.find("span", class_="tested-with").text.strip()
+#             cy = refine_cy(c)
+#         except AttributeError:
+#             cy = ""
+#
+#         data = {
+#             "name": name,
+#             "url": url,
+#             "snippet": snippet,
+#             "active": active,
+#             "cy": cy
+#         }
+#
+#         write_csv(data)
+#
+#
+# def write_csv(data):
+#     with open("plugins1.csv", "a", encoding="windows-1251") as f:
+#         writer = csv.writer(f, delimiter=";", lineterminator="\r")
+#         writer.writerow((data["name"], data["url"], data["snippet"], data["active"], data["cy"]))
+#
+#
+# def main():
+#     for i in range(24, 25):
+#         url = f"https://ru.wordpress.org/plugins/browse/blocks/page/{i}/"
+#         get_data(get_html(url))
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+from parsers import Parser
+
+
+def main():
+    pars = Parser("https://www.ixbt.com/live/index/news/", "news.txt")
+    pars.run()
+
+
+if __name__ == '__main__':
+    main()
