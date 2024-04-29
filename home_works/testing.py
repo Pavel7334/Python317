2df7829656d812937268c82de53b26f0d8c60b79
Pavel7334 <novaya.rabota.pavla2016@yandex.ru>
2024-04-30 01:46:52 +0400

    Домашнее задание №36

--- home_works/testing.py
+++ home_works/testing.py
@@ -1,21 +1,17 @@
-def group_time_slots(time_list):
-    grouped_times = []
-    temp_group = []
-    for time_slot in time_list:
-        temp_group.append(time_slot)
-        if len(temp_group) == 4:
-            grouped_times.append(temp_group)
-            temp_group = []
-    if temp_group:
-        grouped_times.append(temp_group)
-    return grouped_times
+def fibocacci():
+    first = 1
+    second = 1
+    while True:
+        yield first
+        first, second = second, first + second
 
-time_all = [
-    '08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00',
-    '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30',
-    '15:00', '15:30', '16:00', '16:30', '17:00', '17:30',
-    '18:00', '18:30', '19:00'
-]
 
-time_group = group_time_slots(time_all)
-print(time_group)
+gen_obj = fibocacci()
+print(next(gen_obj))
+print(next(gen_obj))
+print(next(gen_obj))
+print(next(gen_obj))
+print(next(gen_obj))
+print(next(gen_obj))
+print(next(gen_obj))
+print(next(gen_obj))
--- main.py
+++ main.py
@@ -6024,7 +6024,7 @@ import re
 #
 #
 # def write_csv(data):
-#     with open("plugins1.csv", "a", encoding="windows-1251") as f:
+#     with open("plugins1.cs", "a", encoding="windows-1251") as f:
 #         writer = csv.writer(f, delimiter=";", lineterminator="\r")
 #         writer.writerow((data["name"], data["url"], data["snippet"], data["active"], data["cy"]))
 #
@@ -6055,4 +6055,79 @@ import re
 # Model - Модель
 # View - вид или представление
 # Controller - контроллер
+    #                                              БД
+# import socket
+# from view import index, blog
+#
+# URLS = {
+#     '/': index,
+#     '/blog': blog,
+# }
+#
+#
+# def parser_request(request):
+#     parsed = request.split()
+#     method = parsed[0]
+#     url = parsed[1]
+#     return method, url
+#
+#
+# def generate_headers(method, url):
+#     if method != 'GET':
+#         return 'HTTP/1.1 405 Method Not Allowed\n\n', 405
+#     if url not in URLS:
+#         return 'HTTP/1.1 404 Not Found\n\n', 404
+#     return 'HTTP/1.1 200 OK!\n\n', 200
+#
+#
+# def generate_content(code, url):
+#     if code == 404:
+#         return f"<h1>404</h1><h3>Page Not Found!</h3>"
+#     if code == 405:
+#         return f"<h1>405</h1><h3>Method Not Allowed</h3>"
+#     return URLS[url]()
+#
+#
+# def generate_response(request):
+#     method, url = parser_request(request)
+#     headers, code = generate_headers(method, url)
+#     body = generate_content(code, url)
+#     return (headers + body).encode()
+#
+#
+# def run():
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server_socket.bind(('127.0.0.1', 5000))
+#     server_socket.listen()
+#
+#     while True:
+#         client_socket, addr = server_socket.accept()
+#         request = client_socket.recv(1024)
+#         print(f"Клиент: {addr} => \n{request.decode('utf-8')}\n")
+#
+#         response = generate_response(request.decode('utf-8'))
+#         client_socket.sendall(response)
+#         client_socket.close()
+#
+#
+# if __name__ == "__main__":
+#     run()
+
+
+import sqlite3
+
+
+# con = sqlite3.connect("profile.db")
+# cur = con.cursor()
+# cur.execute("""""")
+# con.close()
+
+# with sqlite3.connect("profile.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""CREATE TABLE IF NOT EXISTS users(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     summa REAL,
+#     date BLOB)""")
+#     cur.execute("DROP TABLE users")
 
